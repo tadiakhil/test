cc47b4769c470bbd0f36d4f11422e52ae2ed351f
akhil tadi <akhil.tadi@toyota.com>
2021-08-30 12:50:09 +1000

    Added files

--- main.tf
+++ main.tf
@@ -89,10 +89,6 @@ output "s3_website_endpoint" {
   value = "${aws_s3_bucket.site.website_endpoint}"
 }
 
-output "route53_domain" {
-  value = "${aws_route53_record.root_domain.fqdn}"
-}
-
 output "cdn_domain" {
   value = "${aws_cloudfront_distribution.cdn.domain_name}"
 }
