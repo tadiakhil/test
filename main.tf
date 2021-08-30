e05ff5a87f55881ede447b41be623c2ba0a85db6
akhil tadi <akhil.tadi@toyota.com>
2021-08-30 15:15:23 +1000

    added two new files

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ provider "aws" {
 
 # Note: The bucket name needs to carry the same name as the domain!
 # http://stackoverflow.com/a/5048129/2966951
-resource "aws_s3_bucket" "site" {
+resource "aws_s3_bucket" "akhil_cdn_bucket" {
   bucket = "${var.domain}"
   acl = "public-read"
 
@@ -44,28 +44,29 @@ resource "aws_route53_zone" "main" {
 }
 
 resource "aws_route53_record" "root_domain" {
-  zone_id = "${aws_route53_zone.main.zone_id}"
+  zone_id = "${aws_route53_zone.cdn_zone.zone_id}"
   name = "${var.domain}"
   type = "A"
 
   alias {
-    name = "${aws_cloudfront_distribution.cdn.domain_name}"
-    zone_id = "${aws_cloudfront_distribution.cdn.hosted_zone_id}"
+    name = "${aws_cloudfront_distribution.akhil_cdn_distribution.domain_name}"
+    zone_id = "${aws_cloudfront_distribution.akhil_cdn_distribution.hosted_zone_id}"
     evaluate_target_health = false
   }
 }
 
-resource "aws_cloudfront_distribution" "cdn" {
+resource "aws_cloudfront_distribution" "akhil_cdn_distribution" {
   origin {
-    origin_id   = "${var.domain}"
     domain_name = "${var.domain}.s3.amazonaws.com"
+    origin_id   = "${var.domain}"
   }
 
-  # If using route53 aliases for DNS we need to declare it here too, otherwise we'll get 403s.
-  aliases = ["${var.domain}"]
-
   enabled             = true
+  is_ipv6_enabled     = true
+  comment             = "Permanent public file distribution"
   default_root_object = "index.html"
+
+  aliases = ["${var.domain}"]
 
   default_cache_behavior {
     allowed_methods  = ["GET", "HEAD", "OPTIONS"]
@@ -73,26 +74,24 @@ resource "aws_cloudfront_distribution" "cdn" {
     target_origin_id = "${var.domain}"
 
     forwarded_values {
-      query_string = true
+      query_string = false
+
       cookies {
         forward = "none"
       }
     }
 
     viewer_protocol_policy = "allow-all"
-    min_ttl                = 0
-    default_ttl            = 3600
+    min_ttl                = 60
+    default_ttl            = 300
     max_ttl                = 86400
   }
 
-  # The cheapest priceclass
-  price_class = "PriceClass_100"
+  price_class = "PriceClass_All"
 
-  # This is required to be specified even if it's not used.
   restrictions {
     geo_restriction {
       restriction_type = "none"
-      locations        = []
     }
   }
 
@@ -101,14 +100,11 @@ resource "aws_cloudfront_distribution" "cdn" {
   }
 }
 
-output "s3_website_endpoint" {
-  value = "${aws_s3_bucket.site.website_endpoint}"
-}
 
-output "route53_domain" {
-  value = "${aws_route53_record.root_domain.fqdn}"
+output "domain" {
+  value = "${var.domain}"
 }
 
 output "cdn_domain" {
-  value = "${aws_cloudfront_distribution.cdn.domain_name}"
+  value = "${aws_cloudfront_distribution.akhil_cdn_distribution.domain_name}"
 }
