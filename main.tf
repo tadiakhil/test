4d1b524fdedec857ecdfc8a7e28c4c0abe516308
akhil tadi <akhil.tadi@toyota.com>
2021-08-30 17:07:55 +1000

    New Method to host a static site

--- main.tf
+++ main.tf
@@ -1,77 +1,161 @@
-variable "aws_region" {
-  default     = "eu-west-1"
-}
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
 
-variable "domain" {
-  default = "cdn.akhiltadi.com"
+  required_version = ">= 0.12.31"
 }
 
 provider "aws" {
   access_key = "${var.aws_access_key}"
   secret_key = "${var.aws_secret_key}"
-  region = "${var.aws_region}"
+  region  = "us-east-1"
 }
 
-# Note: The bucket name needs to carry the same name as the domain!
-# http://stackoverflow.com/a/5048129/2966951
-resource "aws_s3_bucket" "akhil_cdn_bucket" {
-  bucket = "${var.domain}"
-  acl = "public-read"
+resource "aws_s3_bucket" "mybucket" {
+  bucket = "s3-mybucket-webapp.example.com"
+  acl    = "private"
+  # Add specefic S3 policy in the s3-policy.json on the same directory
+  #policy = file("s3-policy.json")
 
-  policy = <<EOF
-    {
-      "Version":"2008-10-17",
-      "Statement":[{
-        "Sid":"AllowPublicRead",
-        "Effect":"Allow",
-        "Principal": {"AWS": "*"},
-        "Action":["s3:GetObject"],
-        "Resource":["arn:aws:s3:::${var.domain}/*"]
-      }]
-    }
-  EOF
+  versioning {
+    enabled = false
+  }
 
   website {
-      index_document = "index.html"
+    index_document = "index.html"
+    error_document = "error.html"
+	
+	# Add routing rules if required
+    #routing_rules = <<EOF
+    #                [{
+    #                    "Condition": {
+    #                        "KeyPrefixEquals": "docs/"
+    #                    },
+    #                    "Redirect": {
+    #                        "ReplaceKeyPrefixWith": "documents/"
+    #                    }
+    #                }]
+    #                EOF
   }
-}
 
-# Note: Creating this route53 zone is not enough. The domain's name servers need to point to the NS
-# servers of the route53 zone. Otherwise the DNS lookup will fail.
-# To verify that the dns lookup succeeds: `dig site @nameserver`
-resource "aws_route53_zone" "main" {
-  name = "${var.domain}"
-}
-
-resource "aws_route53_record" "root_domain" {
-  zone_id = "${aws_route53_zone.cdn_zone.zone_id}"
-  name = "${var.domain}"
-  type = "A"
-
-  alias {
-    name = "${aws_cloudfront_distribution.akhil_cdn_distribution.domain_name}"
-    zone_id = "${aws_cloudfront_distribution.akhil_cdn_distribution.hosted_zone_id}"
-    evaluate_target_health = false
+  tags = {
+    Environment = "development"
+    Name        = "my-tag"
   }
+
 }
 
-resource "aws_cloudfront_distribution" "akhil_cdn_distribution" {
+#Upload files of your static website
+resource "aws_s3_bucket_object" "html" {
+  for_each = fileset("../../mywebsite/", "**/*.html")
+
+  bucket = aws_s3_bucket.mybucket.bucket
+  key    = each.value
+  source = "../../mywebsite/${each.value}"
+  etag   = filemd5("../../mywebsite/${each.value}")
+  content_type = "text/html"
+}
+
+resource "aws_s3_bucket_object" "svg" {
+  for_each = fileset("../../mywebsite/", "**/*.svg")
+
+  bucket = aws_s3_bucket.mybucket.bucket
+  key    = each.value
+  source = "../../mywebsite/${each.value}"
+  etag   = filemd5("../../mywebsite/${each.value}")
+  content_type = "image/svg+xml"
+}
+
+resource "aws_s3_bucket_object" "css" {
+  for_each = fileset("../../mywebsite/", "**/*.css")
+
+  bucket = aws_s3_bucket.mybucket.bucket
+  key    = each.value
+  source = "../../mywebsite/${each.value}"
+  etag   = filemd5("../../mywebsite/${each.value}")
+  content_type = "text/css"
+}
+
+resource "aws_s3_bucket_object" "js" {
+  for_each = fileset("../../mywebsite/", "**/*.js")
+
+  bucket = aws_s3_bucket.mybucket.bucket
+  key    = each.value
+  source = "../../mywebsite/${each.value}"
+  etag   = filemd5("../../mywebsite/${each.value}")
+  content_type = "application/javascript"
+}
+
+
+resource "aws_s3_bucket_object" "images" {
+  for_each = fileset("../../mywebsite/", "**/*.png")
+
+  bucket = aws_s3_bucket.mybucket.bucket
+  key    = each.value
+  source = "../../mywebsite/${each.value}"
+  etag   = filemd5("../../mywebsite/${each.value}")
+  content_type = "image/png"
+}
+
+resource "aws_s3_bucket_object" "json" {
+  for_each = fileset("../../mywebsite/", "**/*.json")
+
+  bucket = aws_s3_bucket.mybucket.bucket
+  key    = each.value
+  source = "../../mywebsite/${each.value}"
+  etag   = filemd5("../../mywebsite/${each.value}")
+  content_type = "application/json"
+}
+# Add more aws_s3_bucket_object for the type of files you want to upload
+# The reason for having multiple aws_s3_bucket_object with file type is to make sure
+# we add the correct content_type for the file in S3. Otherwise website load may have issues
+
+# Print the files processed so far
+output "fileset-results" {
+  value = fileset("../../mywebsite/", "**/*")
+}
+
+locals {
+  s3_origin_id = "s3-my-webapp.example.com"
+}
+
+resource "aws_cloudfront_origin_access_identity" "origin_access_identity" {
+  comment = "s3-my-webapp.example.com"
+}
+
+resource "aws_cloudfront_distribution" "s3_distribution" {
   origin {
-    domain_name = "${var.domain}.s3.amazonaws.com"
-    origin_id   = "${var.domain}"
+    domain_name = aws_s3_bucket.mybucket.bucket_regional_domain_name
+    origin_id   = local.s3_origin_id
+
+    s3_origin_config {
+      origin_access_identity = aws_cloudfront_origin_access_identity.origin_access_identity.cloudfront_access_identity_path
+    }
   }
 
   enabled             = true
   is_ipv6_enabled     = true
-  comment             = "Permanent public file distribution"
+  comment             = "my-cloudfront"
   default_root_object = "index.html"
 
-  aliases = ["${var.domain}"]
+  # Configure logging here if required 	
+  #logging_config {
+  #  include_cookies = false
+  #  bucket          = "mylogs.s3.amazonaws.com"
+  #  prefix          = "myprefix"
+  #}
+
+  # If you have domain configured use it here 
+  #aliases = ["mywebsite.example.com", "s3-static-web-dev.example.com"]
 
   default_cache_behavior {
-    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+    allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
     cached_methods   = ["GET", "HEAD"]
-    target_origin_id = "${var.domain}"
+    target_origin_id = local.s3_origin_id
 
     forwarded_values {
       query_string = false
@@ -82,17 +166,68 @@ resource "aws_cloudfront_distribution" "akhil_cdn_distribution" {
     }
 
     viewer_protocol_policy = "allow-all"
-    min_ttl                = 60
-    default_ttl            = 300
+    min_ttl                = 0
+    default_ttl            = 3600
     max_ttl                = 86400
   }
 
-  price_class = "PriceClass_All"
+  # Cache behavior with precedence 0
+  ordered_cache_behavior {
+    path_pattern     = "/content/immutable/*"
+    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+    cached_methods   = ["GET", "HEAD", "OPTIONS"]
+    target_origin_id = local.s3_origin_id
+
+    forwarded_values {
+      query_string = false
+      headers      = ["Origin"]
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    min_ttl                = 0
+    default_ttl            = 86400
+    max_ttl                = 31536000
+    compress               = true
+    viewer_protocol_policy = "redirect-to-https"
+  }
+
+  # Cache behavior with precedence 1
+  ordered_cache_behavior {
+    path_pattern     = "/content/*"
+    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = local.s3_origin_id
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+    compress               = true
+    viewer_protocol_policy = "redirect-to-https"
+  }
+
+  price_class = "PriceClass_200"
 
   restrictions {
     geo_restriction {
-      restriction_type = "none"
+      restriction_type = "whitelist"
+      locations        = ["US", "CA", "GB", "DE", "IN", "IR"]
     }
+  }
+
+  tags = {
+    Environment = "development"
+    Name        = "my-tag"
   }
 
   viewer_certificate {
@@ -100,11 +235,33 @@ resource "aws_cloudfront_distribution" "akhil_cdn_distribution" {
   }
 }
 
-
-output "domain" {
-  value = "${var.domain}"
+# to get the Cloud front URL if doamin/alias is not configured
+output "cloudfront_domain_name" {
+  value = aws_cloudfront_distribution.s3_distribution.domain_name
 }
 
-output "cdn_domain" {
-  value = "${aws_cloudfront_distribution.akhil_cdn_distribution.domain_name}"
+data "aws_iam_policy_document" "s3_policy" {
+  statement {
+    actions   = ["s3:GetObject"]
+    resources = ["${aws_s3_bucket.b.arn}/*"]
+
+    principals {
+      type        = "AWS"
+      identifiers = [aws_cloudfront_origin_access_identity.origin_access_identity.iam_arn]
+    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "mybucket" {
+  bucket = aws_s3_bucket.mybucket.id
+  policy = data.aws_iam_policy_document.s3_policy.json
+}
+
+resource "aws_s3_bucket_public_access_block" "mybucket" {
+  bucket = aws_s3_bucket.mybucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  //ignore_public_acls      = true
+  //restrict_public_buckets = true
 }
