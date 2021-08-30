b9534ef4e02de4b1c4cced733e11abd6c18322b4
akhil tadi <akhil.tadi@toyota.com>
2021-08-30 17:37:38 +1000

    New config

--- main.tf
+++ main.tf
@@ -4,9 +4,14 @@ provider "aws" {
   region  = "us-east-1"
 }
 
-resource "aws_s3_bucket" "prod_bucket" {
-    bucket = "test_bucket"
+resource "aws_s3_bucket" "mybucket" {
+    bucket = "${var.bucket_name}"
     acl = "public-read"
+
+    website {
+        redirect_all_requests_to = "index.html"
+    }
+
     cors_rule {
         allowed_headers = ["*"]
         allowed_methods = ["PUT","POST"]
@@ -14,69 +19,66 @@ resource "aws_s3_bucket" "prod_bucket" {
         expose_headers = ["ETag"]
         max_age_seconds = 3000
     }
+
     policy = <<EOF
 {
     "Version": "2008-10-17",
     "Statement": [
-    {
-        "Sid": "PublicReadForGetBucketObjects",
-        "Effect": "Allow",
-        "Principal": {
-            "AWS": "*"
-         },
-         "Action": "s3:GetObject",
-         "Resource": "arn:aws:s3:::test_bucket/*"
-    }, {
-        "Sid": "",
-        "Effect": "Allow",
-        "Principal": {
-            "AWS": "${aws_iam_user.prod_user.arn}"
-        },
-        "Action": "s3:*",
-        "Resource": [
-            "arn:aws:s3:::test_bucket",
-            "arn:aws:s3:::test_bucket/*"
-        ]
-    }]
+        {
+            "Sid": "PublicReadForGetBucketObjects",
+            "Effect": "Allow",
+            "Principal": {
+                "AWS": "*"
+            },
+            "Action": "s3:GetObject",
+            "Resource": "arn:aws:s3:::${var.bucket_name}/*"
+        }
+    ]
 }
-EOF }
+EOF
+}
 
-resource "aws_cloudfront_distribution" "prod_distribution" {
+resource "aws_cloudfront_distribution" "distribution" {
     origin {
-        domain_name = "${aws_s3_bucket.prod.website_endpoint}"
-        origin_id = "S3-${aws_s3_bucket.prod.bucket}"
-        custom_origin_config {
+        domain_name = "${aws_s3_bucket.mybucket.website_endpoint}"
+        origin_id   = "S3-${aws_s3_bucket.mybucket.bucket}"
+
+        custom_origin_config  {
             http_port = 80
             https_port = 443
             origin_protocol_policy = "match-viewer"
             origin_ssl_protocols = ["TLSv1", "TLSv1.1", "TLSv1.2"]
         }
     }
-    # By default, show index.html file
     default_root_object = "index.html"
-    enabled = true
-    # If there is a 404, return index.html with a HTTP 200 Response
+    enabled             = true
+
     custom_error_response {
         error_caching_min_ttl = 3000
-        error_code = 404
-        response_code = 200
-        response_page_path = "/index.html"
+        error_code            = 404
+        response_code         = 200
+        response_page_path    = "/index.html"
     }
+
     default_cache_behavior {
-        allowed_methods = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
-        cached_methods = ["GET", "HEAD"]
-        target_origin_id = "S3-${aws_s3_bucket.prod.bucket}"
-        # Forward all query strings, cookies and headers
+        allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+        cached_methods   = ["GET", "HEAD"]
+        target_origin_id = "S3-${aws_s3_bucket.mybucket.bucket}"
+
         forwarded_values {
             query_string = true
-        }
+
+            cookies {
+                forward = "none"
+            }
+      }
+
         viewer_protocol_policy = "allow-all"
-        min_ttl = 0
-        default_ttl = 3600
-        max_ttl = 86400
+        min_ttl                = 0
+        default_ttl            = 3600
+        max_ttl                = 86400
     }
-    # Distributes content to US and Europe
-    price_class = "PriceClass_100"
+
     # Restricts who is able to access this content
     restrictions {
         geo_restriction {
@@ -84,6 +86,7 @@ resource "aws_cloudfront_distribution" "prod_distribution" {
             restriction_type = "none"
         }
     }
+
     # SSL certificate for the service.
     viewer_certificate {
         cloudfront_default_certificate = true
