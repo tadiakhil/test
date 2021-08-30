76bb5a977446044e61fef8e48a436dcdd9407a3e
akhil tadi <akhil.tadi@toyota.com>
2021-08-30 14:31:22 +1000

    Added keys

--- main.tf
+++ main.tf
@@ -6,7 +6,6 @@ variable "domain" {
   default = "my_domain"
 }
 
-
 provider "aws" {
   region = "${var.aws_region}"
 }
@@ -35,6 +34,24 @@ resource "aws_s3_bucket" "site" {
   }
 }
 
+# Note: Creating this route53 zone is not enough. The domain's name servers need to point to the NS
+# servers of the route53 zone. Otherwise the DNS lookup will fail.
+# To verify that the dns lookup succeeds: `dig site @nameserver`
+resource "aws_route53_zone" "main" {
+  name = "${var.domain}"
+}
+
+resource "aws_route53_record" "root_domain" {
+  zone_id = "${aws_route53_zone.main.zone_id}"
+  name = "${var.domain}"
+  type = "A"
+
+  alias {
+    name = "${aws_cloudfront_distribution.cdn.domain_name}"
+    zone_id = "${aws_cloudfront_distribution.cdn.hosted_zone_id}"
+    evaluate_target_health = false
+  }
+}
 
 resource "aws_cloudfront_distribution" "cdn" {
   origin {
@@ -84,6 +101,10 @@ resource "aws_cloudfront_distribution" "cdn" {
 
 output "s3_website_endpoint" {
   value = "${aws_s3_bucket.site.website_endpoint}"
+}
+
+output "route53_domain" {
+  value = "${aws_route53_record.root_domain.fqdn}"
 }
 
 output "cdn_domain" {
