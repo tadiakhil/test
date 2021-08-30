11ab24eb9a3049ae0b4ebbfd21c4a44fa863ac14
akhil tadi <akhil.tadi@toyota.com>
2021-08-30 12:48:59 +1000

    Added files

--- main.tf
+++ main.tf
@@ -6,6 +6,10 @@ variable "domain" {
   default = "my_domain"
 }
 
+variable "public_key_path" {
+  default = "~/.ssh/id_rsa.pub"
+}
+
 provider "aws" {
   region = "${var.aws_region}"
 }
@@ -34,24 +38,6 @@ resource "aws_s3_bucket" "site" {
   }
 }
 
-# Note: Creating this route53 zone is not enough. The domain's name servers need to point to the NS
-# servers of the route53 zone. Otherwise the DNS lookup will fail.
-# To verify that the dns lookup succeeds: `dig site @nameserver`
-resource "aws_route53_zone" "main" {
-  name = "${var.domain}"
-}
-
-resource "aws_route53_record" "root_domain" {
-  zone_id = "${aws_route53_zone.main.zone_id}"
-  name = "${var.domain}"
-  type = "A"
-
-  alias {
-    name = "${aws_cloudfront_distribution.cdn.domain_name}"
-    zone_id = "${aws_cloudfront_distribution.cdn.hosted_zone_id}"
-    evaluate_target_health = false
-  }
-}
 
 resource "aws_cloudfront_distribution" "cdn" {
   origin {
